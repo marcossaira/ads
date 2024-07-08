c848b6c53b6d591a55e717defe9fab40a1523ace
Carlos Cosme <danielcosme99@gmail.com>
2024-07-08 00:34:55 -0500

    estrategias en un solo archivo

--- emitirComprobanteModule/EstrategiasComprobante.php
+++ emitirComprobanteModule/EstrategiasComprobante.php
@@ -3,12 +3,22 @@ interface CodigoStrategy {
     public function incrementarCodigo($code);
 }
 
+interface FechaStrategy {
+    public function calcularFechaEntrega($current_date);
+}
+
 class IncrementarCodigoProforma implements CodigoStrategy {
     public function incrementarCodigo($code) {
         $code_number = intval(substr($code, 6));
         $code_number++;
         $new_code = substr($code, 0, 6) . strval($code_number);
         return $new_code;
+    }
+}
+
+class FechaEntregaSemana implements FechaStrategy {
+    public function calcularFechaEntrega($current_date) {
+        return date('Y-m-d', strtotime($current_date . ' + 7 days'));
     }
 }
 ?>
